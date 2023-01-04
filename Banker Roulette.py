fea5f9d105662cc1ed9d90cd3737eca63fc0e8dc
Saicharan0611 <76816430+Saicharan0611@users.noreply.github.com>
2023-01-04 21:49:13 +0530

    Update Banker Roulette.py

--- Banker Roulette.py
+++ Banker Roulette.py
@@ -1,11 +1,10 @@
-# Import the random module here
 import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+
+
 num_items = len(names)
 random_choice = random.randint(0, num_items-1)
 person_choosed = names[random_choice]
